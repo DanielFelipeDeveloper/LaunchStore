48b50ca023bcec9d033fea32e5d6205cd176cdc5
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-02-12 16:40:20 -0400

    feat: async await

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,36 @@
+const Category = require('../models/Category');
+const Product = require('../models/Product');
+const Procuts = require('../models/Product');
+
 module.exports = {
   create(req, res) {
-    return res.render("products/create.njk")
+
+    Category.all()
+    .then((results) => {
+      const categories = results.rows;
+
+      return res.render("products/create.njk", { categories });
+    }).catch((err) => {
+      throw new Error(err);
+    });
+  },
+  async post(req, res) {
+    const keys = Object.keys(req.body);
+
+    console.log(req.body)
+
+    for (key of keys)  {
+      if (req.body[key] == "") {
+        return res.send('Please fill all fields!');
+      }
+    }
+
+    let results = await Product.create(req.body);
+    const productId = results.rows[0].id;
+
+    results = await Category.all();
+    const categories = results.rows[0];
+
+    return res.render("products/create.njk", { productId, categories })
   }
 }
