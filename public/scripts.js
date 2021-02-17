bc96cbed490f76f722c9a2f65bfd40594c76eaba
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-02-17 16:51:33 -0400

    feat: refatorando gerenciador de uploads

--- public/scripts.js
+++ public/scripts.js
@@ -16,16 +16,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+  preview: document.querySelector('#photos-preview'), 
   uploadLimit: 6,
   handleFileInput(event) {
     const { files: fileList } = event.target;
-    const { uploadLimit } = PhotosUpload;
 
-    if (fileList.length > uploadLimit) {
-      alert(`Envie no máximo ${uploadLimit} fotos!!`);
-      event.preventDefault();
-      return
-    }
+    if(PhotosUpload.hasLimit(event, fileList)) return;
 
     Array.from(fileList).forEach(file => {
       const reader = new FileReader();
@@ -34,17 +30,32 @@ const PhotosUpload = {
         const image = new Image();
         image.src = String(reader.result);
 
-        const container = document.createElement('div');
-        container.classList.add('photo');
-
-        container.onclick = () => alert('remover foto');
-
-        container.appendChild(image);
-        
-        document.querySelector('#photos-preview').appendChild(container);
+        const container = PhotosUpload.getContainer(image);
+        PhotosUpload.preview.appendChild(container);
       }
 
       reader.readAsDataURL(file);
     });
+  },
+  hasLimit(event, fileList) {
+    const { uploadLimit } = PhotosUpload;
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos!!`);
+      event.preventDefault();
+      return true;
+    }
+
+    return false;
+  },
+  getContainer(image) {
+    const container = document.createElement('div');
+    container.classList.add('photo');
+
+    container.onclick = () => alert('remover foto');
+
+    container.appendChild(image);
+
+    return container;
   }
 }
