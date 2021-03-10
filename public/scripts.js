92fc6107495257af8e0cead9edecb23f9e49cdbd
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-03-10 15:35:48 -0400

    feat: regra de limita maximo de envio

--- public/scripts.js
+++ public/scripts.js
@@ -48,10 +48,25 @@ const PhotosUpload = {
     PhotosUpload.getAllFiles();
   },
   hasLimit(event) {
-    const { uploadLimit, input: fileList } = PhotosUpload;
+    const { uploadLimit, input, preview } = PhotosUpload;
+    const { files: fileList } = input;
 
     if (fileList.length > uploadLimit) {
       alert(`Envie no máximo ${uploadLimit} fotos!!`);
+      event.preventDefault();
+      return true;
+    }
+
+    const photosDiv = []
+    preview.childNodes.forEach(item => {
+      if (item.classList && item.classList.value == "photo") 
+        photosDiv.push(item);
+    });
+
+    const totalPhotos = fileList.length + photosDiv.length;
+
+    if (totalPhotos > uploadLimit) {
+      window.alert("Você atingiu o limite máximo de fotos");
       event.preventDefault();
       return true;
     }
