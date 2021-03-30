ae3741ca084e7eee8c0b5516d18a067422ef1a64
Daniel Felipe Developer <danielfelipedeveloper@gmail.com>
2021-03-30 08:40:20 -0400

    feat: salvando excluindo e atualizando imagens

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require('../../lib/utils');
 
 const Category = require('../models/Category');
 const Product = require('../models/Product');
+const File = require('../models/File');
 
 module.exports = {
   create(req, res) {
@@ -24,21 +25,48 @@ module.exports = {
       }
     }
 
+    if (req.files.length == 0) 
+      return res.send('Please, send at lesat one image');
+ 
     let results = await Product.create(req.body);
     const productId = results.rows[0].id;
 
-    results = await Category.all();
-    const categories = results.rows;
-
-    return res.redirect(`/products/${productId}/edit`, { productId, categories })
+    const filesPromise = req.files.map(file => File.create(
+      file.filename, 
+      file.path, 
+      productId
+    ));
+    await Promise.all(filesPromise);
+    
+    res.redirect(`/products/${productId}/edit`)
   },
   async put(req, res) {
     const keys = Object.keys(req.body);
 
+    console.log(req.body)
+
     for (key of keys) {
-      if (req.body[key] == "") {
+      if (req.body[key] == "" && key != "removed_files") {
         return res.send('Please, fill all fields!');
       }
+    }
+
+    if (req.files.length != 0) {
+      const newFilesPromise = req.files.map(file => 
+        File.create(file.filename, file.path, req.body.id)
+      )
+
+      await Promise.all(newFilesPromise);
+    }
+
+    if (req.body.removed_files) {
+      const removedFiles = req.body.removed_files.split(',');
+      const lastIndex = removedFiles.length - 1;
+      removedFiles.splice(lastIndex, 1);
+
+      const removedFilesPromise = removedFiles.map(id => File.delete(id));
+
+      await Promise.all(removedFilesPromise);
     }
 
     req.body.price = req.body.price.replace(/\D/g, "");
@@ -69,6 +97,14 @@ module.exports = {
     results = await Category.all();
     const categories = results.rows;
 
-    return res.render("products/edit.njk", { product, categories })
+    results = await Product.files(product.id);
+
+    let files = results.rows;
+    files = files.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+    }));
+
+    return res.render("products/edit.njk", { product, categories, files });
   }
 }
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg")
 
 module.exports = new Pool({
   user: "postgres",
-  password: "2801",
+  password: "docker",
   host: "localhost",
   port: 5432,
   database: "launchstoredb"
