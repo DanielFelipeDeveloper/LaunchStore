40167869239669ba7a0815efa7bd3234d314e1f6
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-05-18 21:35:03 -0400

    feat: mascara de cpf cnpj e cp

--- public/scripts.js
+++ public/scripts.js
@@ -12,6 +12,30 @@ const Mask = {
       currency: 'BRL'
     }).format(value / 100)
 
+  },
+  cpfCnpj(value) {
+    value = value.replace(/\D/g,"")
+
+    if (value.length > 14) value = value.slice(0, -1)
+
+    // check if is cnpj
+    if (value.length > 11) {
+      value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5")
+    } else {
+      // cpf
+      value = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+    }
+
+    return value
+  },
+  cep(value) {
+    value = value.replace(/\D/g,"")
+
+    if (value.length > 8) value = value.splice(0, -1)
+
+    value = value.replace(/(\d{5})(\d{3})/, "$1-$2")
+
+    return value
   }
 }
 
