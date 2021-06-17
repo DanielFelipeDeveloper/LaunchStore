a4e6da6821d58f46f536ffd54435c1600ac8ddec
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-06-16 22:10:58 -0400

    feat: validacao do email

--- public/scripts.js
+++ public/scripts.js
@@ -176,3 +176,39 @@ const Lightbox = {
     Lightbox.closeButton.style.top = "-80px";
   }
 }
+
+const Validate = {
+  apply(input, func) {
+    Validate.clearErrors(input)
+
+    let { error, value } = Validate[func](input.value);
+    input.value = value;
+
+    if (error) Validate.displayError(input, error);
+
+  },
+  displayError(input, error) {
+    const div = document.createElement('div');
+    div.classList.add('error');
+    div.innerHTML = error;
+    input.parentNode.appendChild(div);
+    input.focus();
+  },
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector(".error");
+
+    if (errorDiv) errorDiv.remove();
+  },
+  isEmail(value) {
+    let error = null;
+
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+    if (!value.match(mailFormat)) error = "Email inválido";
+
+    return {
+      error,
+      value
+    }
+  }
+}
